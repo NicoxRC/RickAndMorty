00ead862b36c2954414c1e51fe28a20e8e262ebb
NicoxRC <nicorojas005@gmail.com>
2023-02-20 10:33:29 -0500

    some routes

--- RickAndMortyFront/src/App.tsx
+++ RickAndMortyFront/src/App.tsx
@@ -1,5 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
+import CharacterDetails from './pages/characterDetails/characterDetails';
+import NewCharacter from './pages/newCharacter/NewCharacter';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -8,6 +10,8 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/details" element={<CharacterDetails />} />
+        <Route path="/newCharacter" element={<NewCharacter />} />
       </Routes>
     </div>
   );
