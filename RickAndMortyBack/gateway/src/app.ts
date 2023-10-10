f3dc147e8a49f5f9b5a81bf5f96d19a25f769362
NicoxRC <nicorojas005@gmail.com>
2023-10-10 16:30:47 -0500

    gateway complete

--- RickAndMortyBack/gateway/src/app.ts
+++ RickAndMortyBack/gateway/src/app.ts
@@ -1,11 +1,31 @@
 import express from 'express';
 import morgan from 'morgan';
-import routes from './routes/index.routes';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(routes);
+app.use(
+  '/characters',
+  createProxyMiddleware({
+    target: 'http://localhost:3002',
+    changeOrigin: true,
+  })
+);
+app.use(
+  '/locations',
+  createProxyMiddleware({
+    target: 'http://localhost:3003',
+    changeOrigin: true,
+  })
+);
+app.use(
+  '/episodes',
+  createProxyMiddleware({
+    target: 'http://localhost:3004',
+    changeOrigin: true,
+  })
+);
 
 export default app;
