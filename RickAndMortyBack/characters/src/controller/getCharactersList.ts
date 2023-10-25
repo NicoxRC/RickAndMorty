4f37c5ee995154bf4e9448de5419e750437d540c
NicoxRC <nicorojas005@gmail.com>
2023-10-24 19:20:30 -0500

    filters complete

--- RickAndMortyBack/characters/src/controller/getCharactersList.ts
+++ RickAndMortyBack/characters/src/controller/getCharactersList.ts
@@ -4,8 +4,28 @@ import type { CharactersInterface } from '../interfaces/characters';
 
 export = async (req: Request, res: Response): Promise<void> => {
   try {
+    const { gender, status, species, type } = req.query;
+
+    let genderUrl: string = '';
+    let statusUrl: string = '';
+    let speciesUrl: string = '';
+    let typeUrl: string = '';
+
+    if (typeof gender == 'string' && gender.length > 0) {
+      genderUrl = gender;
+    }
+    if (typeof status == 'string' && status.length > 0) {
+      statusUrl = status;
+    }
+    if (typeof species == 'string' && species.length > 0) {
+      speciesUrl = species;
+    }
+    if (typeof type == 'string' && type.length > 0) {
+      typeUrl = type;
+    }
+
     const { data } = await axios.get<CharactersInterface[]>(
-      'http://database:3005/characters'
+      `http://database:3005/characters?gender=${genderUrl}&status=${statusUrl}&species=${speciesUrl}&type=${typeUrl}`
     );
     res.status(200).json(data);
   } catch (error: unknown) {
